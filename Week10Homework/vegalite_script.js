7b6b8735458ae3bc9c7f70257cebf2965b951377
riaraghani <79733413+riaraghani@users.noreply.github.com>
2022-10-09 21:14:21 +1100

    Add files via upload

---
+++ Week10Homework/vegalite_script.js
@@ -0,0 +1,9 @@
+var vg_1 = "map.vg.json";
+vegaEmbed("#map", vg_1).then(function(result) {
+    // Access the Vega view instance
+    }).catch(console.error);
+
+var vg_2 = "map1.vg.json";
+vegaEmbed("#map1", vg_2).then(function(result) {
+    // Access the Vega view instance
+    }).catch(console.error);
